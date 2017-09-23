c00d03ae897cf9e02c0a46d20e858ced4449132d
Coni63 <mine.nicolas@gmail.com>
2017-09-23 14:24:51 +0200

    amélioration du lav v2 avec multithreaded client and server

--- Python_Scripts/openclassroom/lab v2/client.py
+++ Python_Scripts/openclassroom/lab v2/client.py
@@ -1,24 +1,56 @@
 import socket
+import threading
 
-def open_connection(host, port):
-    socket_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_.connect((host, port))
-    return socket_
+class ThreadedClient(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.connect((self.host, self.port))
 
-def main():
-    connexion = open_connection(hote, port)
-    while True:
-        txt = input('test')
-        if txt == "Q":
-            break
-        else:
-            connexion.send(txt.encode())
-    connexion.close()
+    def connect(self):
+        size = 1024
+        while True:
+            try:
+                data = self.sock.recv(size)
+                if data:
+                    response = data
+                    if response == b"Max Player limit reached":
+                        print("La limite de joueur a été atteinte, vous ne pouvez pas vous connecter au serveur")
+                        self.sock.close()
+                    else:
+                        print(response.decode())
+                        self.listener = threading.Thread(target=self.listenToServer).start()
+                        self.sender = threading.Thread(target=self.listenToClient).start()
+            except:
+                self.sock.close()
+
+
+    def listenToServer(self):
+        size = 1024
+        while True:
+            try:
+                data = self.sock.recv(size)
+                if data:
+                    response = data
+                    print(response)
+            except:
+                self.sock.close()
+
+    def listenToClient(self):
+        while True:
+            txt = input('test')
+            if txt == "Q":
+                self.sock.close()
+                break
+            else:
+                self.sock.send(txt.encode())
 
 
 if __name__ == "__main__":
     hote = "localhost"
     port = 12800
-    main()
+    ThreadedClient(hote, port).connect()
 
 
--- Python_Scripts/openclassroom/lab v2/server.py
+++ Python_Scripts/openclassroom/lab v2/server.py
@@ -1,21 +1,69 @@
+import datetime
+import time
 import socket
+import threading
+
+
+class ThreadedServer(object):
+    LIMIT = 2
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+        self.player=[None]*self.LIMIT
+
+    @property
+    def nb_player(self):
+        return len(list(filter(lambda x: x is not None, self.player)))
+
+    def remove_user(self, user):
+        self.player[self.player.index(user)] = None
+        user.close()
+        print("user disconnected")
+
+    def listen(self):
+        self.sock.listen(5)
+        print("Serveur lancé, en attente de connexion")
+        threading.Thread(target=self.status).start()
+        while True:
+            client, address = self.sock.accept()
+            print(self.nb_player)
+            if self.nb_player == self.LIMIT:
+                print("rejet de", client)
+                client.send(b"Max Player limit reached")
+                client.close()
+            else:
+                num = self.player.index(None)
+                self.player[num] = client
+                client.send(("Bonjour Player_{}".format(num+1)).encode())
+                # client.settimeout(60)
+                threading.Thread(target = self.listenToClient, args = (client, address)).start()
+
+    def status(self):
+        while True:
+            time.sleep(3)
+            print("{} : {} client connected".format(datetime.datetime.now(), self.nb_player))
+
+    def listenToClient(self, client, address):
+        size = 1024
+        while True:
+            try:
+                data = client.recv(size)
+                if data:
+                    # Set the response to echo back the recieved data
+                    response = data
+                    print(client, response)
+                else:
+                    self.remove_user(client)
+                    break
+            except:
+                self.remove_user(client)
+                break
+
 
 if __name__ == "__main__":
     hote = ''
     port = 12800
-
-    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.bind((hote, port))
-
-
-    while True:
-        socket.listen(5)
-        client, address = socket.accept()
-        print("{} connected".format(address))
-        response = client.recv(255)
-        if response != "":
-            print(response)
-
-    print("Close")
-    client.close()
-    socket.close()
+    ThreadedServer(hote, port).listen()
