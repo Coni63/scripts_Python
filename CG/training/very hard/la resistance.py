9da69e32612f108bb7a127a542bb7b922349f0a1
Coni63 <mine.nicolas@gmail.com>
2017-09-24 09:51:11 +0200

    update resistance

--- CG/training/very hard/la resistance.py
+++ CG/training/very hard/la resistance.py
@@ -1,12 +1,17 @@
 import sys
 import math
 import re
+import time
 
-morse = {"A":".-", "B":"-...", "C":"-.-.", "D":"-..", "E":".", "F":"..-.", "G":"--.", "H":"....", "I":"..", "J":".---", "K":"-.-", "L":".-..", "M":"--", "N":"-.", "O":"---", "P":".--.", "Q":"--.-", "R":".-.", "S":"...", "T":"-", "U":"..-", "V":"...-", "W":".--", "X":"-..-", "Y":"-.--", "Z":"--.."} 
+morse = {"A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.", "G": "--.", "H": "....", "I": "..",
+         "J": ".---", "K": "-.-", "L": ".-..", "M": "--", "N": "-.", "O": "---", "P": ".--.", "Q": "--.-", "R": ".-.",
+         "S": "...", "T": "-", "U": "..-", "V": "...-", "W": ".--", "X": "-..-", "Y": "-.--", "Z": "--.."}
 dico = {}
 
 l = input()
-print(l, file=sys.stderr)  
+print(l, file=sys.stderr)
+
+s = time.time()
 
 n = int(input())
 for i in range(n):
@@ -16,13 +21,27 @@ for i in range(n):
     for each in w:
         conv += morse[each]
     dico[w] = conv
-print(dico, file=sys.stderr)
-    
-count = 0
-for each in dico.keys():
-    if dico[each] == l:
-        count +=1
 
+print(dico, file=sys.stderr)
+
+print(time.time() - s, file=sys.stderr)
+
+count = 0
+t = l[:]
+r = None
+while r != "":
+    r = ""
+    while True:
+        for key, code in dico.items():
+            if code == t[:len(code)]:
+                t = t[len(code):]
+                r += key
+                del dico[key]
+                break  # for
+        if len(t) == 0:
+            count += 1
+            break
+    print(r, file=sys.stderr)
 print(count)
 
 # To debug: print("Debug messages...", file=sys.stderr)
